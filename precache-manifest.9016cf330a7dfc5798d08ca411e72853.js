bdaa16418caca2868956f8285cd633730297d886
guigaoliveira <gmbo182@hotmail.com>
2020-01-03 08:06:35 -0300

    Updates

---
+++ precache-manifest.9016cf330a7dfc5798d08ca411e72853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c748b46c103398888ba617787702fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e351592bf89f74a97cd",
+    "url": "/static/css/main.580afc9a.chunk.css"
+  },
+  {
+    "revision": "1fb5226ae7aa59ca1920",
+    "url": "/static/js/2.bc9f12ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bc9f12ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e351592bf89f74a97cd",
+    "url": "/static/js/main.d44070ef.chunk.js"
+  },
+  {
+    "revision": "da8a2a4165c75e744291",
+    "url": "/static/js/runtime-main.406d073d.js"
+  }
+]);
