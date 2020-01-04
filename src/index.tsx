8e2b78f29fee98201b04fbf11383399176db435a
guigaoliveira <gmbo182@hotmail.com>
2020-01-04 17:32:02 -0300

    byebye twitto

--- src/index.tsx
+++ src/index.tsx
@@ -29,9 +29,7 @@ function App() {
     <div className="App">
       <main className="container">
         <h1 className="title">Hi, I'm Guilherme Oliveira</h1>
-        <h4 className="phrase">
-          I like to create amazing things for other people
-        </h4>
+        <h4 className="phrase">I like to create amazing things for others</h4>
         <div className="icons">
           <IconContext.Provider
             value={{
@@ -40,18 +38,17 @@ function App() {
           >
             <Link href="https://github.com/guigaoliveira" Icon={FaGithub} />
             <Link href="https://medium.com/@guigaoliveira_" Icon={FaMedium} />
-            <Link href="https://twitter.com/d4mmit" Icon={FaTwitter} />
             <Link
               href="https://linkedin.com/in/guilherme-oliveira1/"
               Icon={FaLinkedinIn}
             />
             <Link
-              href="https://telegram.me/guigaoliveira"
-              Icon={FaTelegramPlane}
-            />
-            <Link
               href="https://pt.slideshare.net/GuilhermeOliveira540"
               Icon={FaSlideshare}
+            />
+            <Link
+              href="https://telegram.me/guigaoliveira"
+              Icon={FaTelegramPlane}
             />
           </IconContext.Provider>
         </div>
